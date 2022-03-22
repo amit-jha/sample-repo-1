8a584526b8a11fe32cc8efa505a4f2e705c126aa
amit-jha <my-public-repo@gmail.com>
2022-03-22 10:24:07 +0530

    c

--- src/main/java/com/aj/searchapi/controller/SearchController.java
+++ src/main/java/com/aj/searchapi/controller/SearchController.java
@@ -12,6 +12,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -55,7 +57,9 @@ public class SearchController {
     }
 
     @GetMapping("/")
-    public Response health() {
-        return Response.builder().status("UP").build();
+    public ResponseEntity health() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Response.builder().status("UP").build());
+
     }
 }
