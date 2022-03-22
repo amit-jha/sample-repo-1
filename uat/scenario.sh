e0cf8d843e76cd33999164db3abd2862dbd53781
amit-jha <my-public-repo@gmail.com>
2022-03-22 10:17:26 +0530

    c

--- uat/scenario.sh
+++ uat/scenario.sh
@@ -9,7 +9,9 @@ sleep 5
 #ping app url
 status_code=$(curl -s -o /dev/null -w "%{http_code}" localhost:${port}/)
 
-if [ ${status_code} == 200 ]
+echo ${status_code}
+
+if [ ${status_code} == 200 ];
 then
   echo "PASS: ${hostname}:${port} is reachable"
 else
